2a3e98de010421c6b81d97b2c9eb68e7b6d5a33e
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2023-04-07 12:58:01 -0300

    Implementada Lógica do Cálculo

--- app/src/main/java/devandroid/gkp/appgaseta/apoio/UtilGasEta.java
+++ app/src/main/java/devandroid/gkp/appgaseta/apoio/UtilGasEta.java
@@ -1,4 +1,29 @@
 package devandroid.gkp.appgaseta.apoio;
 
 public class UtilGasEta {
+
+    public void metodoNaoEstatico(){}
+
+    public static void metodoEstatico(){}
+
+    public static String mensagem(){
+        return "Qualquer mensagem";
+    }
+
+    public static double valor(){
+        return 599.99;
+    }
+
+    public static String calcularMelhorOpcao(double gasolina, double etanol){
+        double precoIdeal = gasolina * 0.70;
+        String mensagemDeRetorno;
+
+        if (etanol <= precoIdeal){
+            mensagemDeRetorno = "Abasteça com Etanol";
+        }
+        else {
+            mensagemDeRetorno = "Abasteça com Gasolina";
+        }
+        return mensagemDeRetorno;
+    }
 }
--- app/src/main/java/devandroid/gkp/appgaseta/view/GasEtaActivity.java
+++ app/src/main/java/devandroid/gkp/appgaseta/view/GasEtaActivity.java
@@ -1,4 +1,29 @@
 package devandroid.gkp.appgaseta.view;
 
-public class GasEtaActivity {
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import devandroid.gkp.appgaseta.R;
+import devandroid.gkp.appgaseta.apoio.UtilGasEta;
+
+public class GasEtaActivity extends AppCompatActivity {
+
+   // UtilGasEta utilGasEta = new UtilGasEta();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_gaseta);
+
+       // utilGasEta.metodoNaoEstatico();
+
+        // UtilGasEta.metodoEstatico();
+
+        Toast.makeText(GasEtaActivity.this, UtilGasEta.calcularMelhorOpcao(5.55,3.19) ,
+                Toast.LENGTH_LONG).show();
+    }
 }
